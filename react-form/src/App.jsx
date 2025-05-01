28967a4c6a23751451ade329a339386aeda60781
RehanFaryad-2802 <seoneoptmizer@gmail.com>
2025-05-01 19:11:48 +0500

    rechecked react form

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const formFileds = {
+  // username: "",
+  // email: "",
+  // password: "",
+  // number: "",
+  // };
+  // const [form, setForm] = useState(formFileds);
+  const [form, setForm] = useState({});
 
+  let handleForm = (e) => setForm({ ...form, [e.target.type]: e.target.value });
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <input
+          onChange={handleForm}
+          value={form.text || ""}
+          type="text"
+          // name="text" // username
+        />
+        <input
+          onChange={handleForm}
+          value={form.email || ""}
+          type="email"
+          // name="email"
+        />
+        <input
+          onChange={handleForm}
+          value={form.date || ""}
+          type="date"
+          // name="date"
+        />
+      </form>
+      <p>{form.text}</p>
+      <p>{form.email}</p>
+      <p>{form.date}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
